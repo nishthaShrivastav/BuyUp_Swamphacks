be6e4eac5a46e4551b7e654c7df48e9836068529
Nishtha Shrivastav <nshrivastav@ufl.edu>
2019-01-19 08:54:55 -0500

    app_related[NS]

--- app/src/main/java/com/example/buyupproject/MainActivity.java
+++ app/src/main/java/com/example/buyupproject/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.buyupproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1 = findViewById(R.id.button1);
+        Button b2 = findViewById(R.id.button2);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SupermarketLocation.class));
+
+            }
+        });
+
     }
+
 }
