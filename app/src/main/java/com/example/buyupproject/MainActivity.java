429f7943551b86ea41746e62ca5e3ec125c0c37d
sonal <sonal.joshi81094@gmail.com>
2019-01-19 21:00:37 -0500

    database[SJ]

--- app/src/main/java/com/example/buyupproject/MainActivity.java
+++ app/src/main/java/com/example/buyupproject/MainActivity.java
@@ -3,18 +3,33 @@ package com.example.buyupproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "NewPostActivity";
+    private static final String REQUIRED = "Required";
 
+    // [START declare_database_ref]
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         Button b1 = findViewById(R.id.button1);
         Button b2 = findViewById(R.id.button2);
 
@@ -30,10 +45,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SupermarketLocationActivity.class));
-
+                submitPost();
             }
         });
-
     }
+    private void submitPost() {
+        final String userId = "test";
+        mDatabase.child("users").child(userId).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        String value = dataSnapshot.getValue(String.class);
 
+                            // User is null, error out
+                            Log.e(TAG, "Inserted " + value );
+
+                            writeNewPost();
+                        finish();
+                        // [END_EXCLUDE]
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                        // [START_EXCLUDE
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    private void writeNewPost() {
+        String key = mDatabase.child("posts").push().getKey();
+        String post = "Testing";
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/posts/" + key, post);
+        mDatabase.updateChildren(childUpdates);
+    }
 }
